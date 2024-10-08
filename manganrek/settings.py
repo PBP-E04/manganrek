55499753a974e5ecded7a7021a6f74ffd7c0ca35
Anders35 <andersleo05@gmail.com>
2024-10-08 18:18:10 +0700

    add github actions

--- manganrek/settings.py
+++ manganrek/settings.py
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://anders-willard-manganrek.pbp.cs.ui.ac.id/", "https://anders-willard-manganrek.pbp.cs.ui.ac.id/"]
