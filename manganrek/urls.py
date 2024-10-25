cc8ef4cd71961f8314902392de02e67d8cea923f
Anders35 <andersleo05@gmail.com>
2024-10-25 23:33:36 +0700

    add main page

--- manganrek/urls.py
+++ manganrek/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
     path('restoran-makanan/', include('restoran_makanan.urls')),
 ]
